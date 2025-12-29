5de5f476cb90f43d24e0351be4288e0e52ba3a12
David <david.akhmedbayev@icloud.com>
2025-12-29 18:33:46 -0500

    Initial Commit

---
+++ Dualsense/DualsenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DualsenseApp.swift
+//  Dualsense
+//
+//  Created by David Akhmedbayev on 12/29/25.
+//
+
+import SwiftUI
+
+@main
+struct DualsenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
