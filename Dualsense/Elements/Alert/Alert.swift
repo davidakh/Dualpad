e5ee816a702735723508dabb9bc96d8f1a8aa35a
David <david.akhmedbayev@icloud.com>
2025-12-31 21:29:24 -0500

    Made the app a agent.

---
+++ Dualsense/Elements/Alert/Alert.swift
@@ -0,0 +1,18 @@
+//
+//  Alert.swift
+//  Dualsense
+//
+//  Created by David Akhmedbayev on 12/31/25.
+//
+
+import SwiftUI
+
+struct Alert: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Alert()
+}
---
+++ Dualsense/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Dualsense
+//
+//  Created by David Akhmedbayev on 12/31/25.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
