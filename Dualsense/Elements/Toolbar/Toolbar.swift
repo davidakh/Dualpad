35f5fe643394df005fd2a26dcfa11fad17d76e85
David <david.akhmedbayev@icloud.com>
2026-01-03 17:55:04 -0500

    Improved toolbar and menu interaction.

--- Dualsense/Elements/Toolbar/Toolbar.swift
+++ Dualsense/Elements/Toolbar/Toolbar.swift
@@ -2,44 +2,17 @@
 //  Toolbar.swift
 //  Dualsense
 //
-//  Created by David Akhmedbayev on 12/29/25.
+//  Created by David Akhmedbayev on 1/3/26.
 //
 
 import SwiftUI
 
 struct Toolbar: View {
-    @Binding var name: String
-    var onBack: () -> Void
-    
     var body: some View {
-        HStack {
-            Button(action: {
-                withAnimation {
-                    onBack()
-                }
-            }) {
-                Image(systemName: "chevron.left")
-                    .font(.system(size: 10, weight: .heavy))
-                    .offset(x: 0.5)
-                    .frame(width: 20, height: 20)
-                    .background(Color.fill)
-                    .clipShape(Circle())
-            }
-            .buttonStyle(.plain)
-            
-            Spacer()
-            
-            Text(name)
-                .font(.headline)
-                .fontWeight(.semibold)
-            
-            Spacer()
-            
-            Rectangle()
-                .frame(width: 20, height: 20)
-                .foregroundStyle(.clear)
-        }
-        .padding(6)
-        .frame(height: 32)
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
+}
+
+#Preview {
+    Toolbar()
 }
